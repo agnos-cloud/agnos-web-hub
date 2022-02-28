9a70539063661d3e4fded1d2b5acd3962d29e9e8
chieze-franklin <chieze.franklin@gmail.com>
2022-02-28 15:55:25 +0100

    fix express issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import path from "path";
 const PORT = process.env.PORT || 5000;
 
 express()
-    .use(express.static(path.join(__dirname, "dist")))
+    .use(express.static(path.join(process.cwd(), "dist")))
     .get("*", (req, res) => {
         res.sendFile("index.html", { root: "dist" });
     })
